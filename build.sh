4f90dd7817a220dbf283af8c6deeb0714c4abe35
Julius Leppälä <julius.leppala@fns.fi>
2021-08-27 14:55:25 +0300

    Update images

--- build.sh
+++ build.sh
@@ -2,3 +2,9 @@
 
 docker build --rm -t juliusleppala/node-browsers:latest-centos -f Dockerfile-centos .
 docker build --rm -t juliusleppala/node-browsers:latest-ubuntu -f Dockerfile-ubuntu .
+
+docker tag juliusleppala/node-browsers:latest-ubuntu juliusleppala/node-browsers:latest
+
+docker push juliusleppala/node-browsers:latest-centos
+docker push juliusleppala/node-browsers:latest-ubuntu
+docker push juliusleppala/node-browsers:latest
