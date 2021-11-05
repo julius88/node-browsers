a2f21ae686be50e2888d76145c161d17c3beb853
Julius Leppälä <julius.leppala@fns.fi>
2021-11-05 18:16:29 +0200

    Update node-browsers
    Use only Ubuntu-based image. Enabled GitHub actions.

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-docker build --rm -t juliusleppala/node-browsers:latest-centos -f Dockerfile-centos .
-docker build --rm -t juliusleppala/node-browsers:latest-ubuntu -f Dockerfile-ubuntu .
+docker build --rm -t juliusleppala/node-browsers:latest-ubuntu -f Dockerfile .
 
 docker tag juliusleppala/node-browsers:latest-ubuntu juliusleppala/node-browsers:latest
 
-docker push juliusleppala/node-browsers:latest-centos
 docker push juliusleppala/node-browsers:latest-ubuntu
 docker push juliusleppala/node-browsers:latest
